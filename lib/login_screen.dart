f37cd8da8b3c660336c53c0e17d42e51e85f1549
HNaumburg <helena.naumburg@gmail.com>
2023-10-13 11:36:40 +0200

    loginpage done!

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:codoc/signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -16,114 +17,129 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        '/signup': (context) => SignUpScreen(),
+      },
       title: 'CoDoc',
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: Scaffold(
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                //Text
-                Text(
-                  'CoDoc',
-                  style: TextStyle(
-                    fontSize: 60,
-                    fontWeight: FontWeight.w300,
+      home: MyLoginScreen(),
+    );
+  }
+}
+
+class MyLoginScreen extends StatefulWidget {
+  const MyLoginScreen({super.key});
+
+  @override
+  State<MyLoginScreen> createState() {
+    return _MyLoginScreenState();
+  }
+}
+
+class _MyLoginScreenState extends State<MyLoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              //Text
+              Text(
+                'CoDoc',
+                style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+
+              // username,
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 26, top: 24, right: 26, bottom: 24.00),
+                child: TextFormField(
+                  //controller: controller,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Username",
                   ),
                 ),
+              ),
 
-                // username,
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 26, right: 26, bottom: 24.00),
-                  child: TextField(
-                    //controller: controller,
-                    obscureText: false,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Username",
-                    ),
+              // password,
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: 26, right: 26, bottom: 24.00),
+                child: TextFormField(
+                  //controller: controller,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Password",
                   ),
                 ),
+              ),
 
-                // password,
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 26, right: 26, bottom: 24.00),
-                  child: TextField(
-                    //controller: controller,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Password",
-                    ),
-                  ),
-                ),
-
-                //Login Button
-                SizedBox(
-                  // width: 300.0,
-                  height: 40.0,
-                  child: FilledButton(
-                    onPressed: () {}, //WHY DO I NEED NULL????
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                        // fontSize: 20,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                  ),
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.only(
-                    bottom: 24.00,
-                  ),
-                ),
-
-                //Text (Or log in with)
-                Padding(
-                  padding: EdgeInsets.only(
-                    bottom: 24.0,
-                  ),
+              //Login Button
+              SizedBox(
+                // width: 300.0,
+                height: 40.0,
+                child: FilledButton(
+                  onPressed: () {}, //WHY DO I NEED NULL????
                   child: Text(
-                    'Or log in with',
+                    'Login',
+                    style: TextStyle(
+                      // fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                    ),
                   ),
                 ),
+              ),
 
-                Padding(
-                  padding: EdgeInsets.only(
-                    bottom: 24.0,
-                  ),
-                  child: Icon(
-                    Icons.beach_access,
-                    color: Colors.blue,
-                    size: 36.0,
-                  ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 40.00,
                 ),
-              ],
-            ),
+              ),
+
+              //Text (Or log in with)
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: 10.0,
+                ),
+                child: Text(
+                  'Or log in with',
+                ),
+              ),
+
+              Image.asset(
+                'assets/images/googleicon.png', // Replace with the path to your image file
+                width: 50, // Set the width of the image
+                height: 50, // Set the height of the image
+              ),
+
+              //Signup
+              const SizedBox(height: 10),
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 15),
+                ),
+                onPressed: () {
+                  print('Signup button tapped'); //Debugger
+
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignUpScreen()));
+                },
+                child: const Text('Sign up here'),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
-//Widget _padding() {}
-
-
-             // Padding(
-                //   padding: EdgeInsets.only(
-                //       left: 27.0, top: 30, right: 27.0, bottom: 24),
-                //   child: TextField(
-                //     decoration: InputDecoration(
-                //       border: OutlineInputBorder(),
-                //       labelText: 'Username',
-                //     ),
-                //   ),
-                // ),    
