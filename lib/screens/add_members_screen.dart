9353551ba54e228d544c2c6dccf4e84bed8d5dbb
Jonathan Stigson <jonathan.stigson@gmail.com>
2023-10-25 09:54:28 +0200

    Create file for adding members page

---
+++ lib/screens/add_members_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddMembersPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AddMembersPageState();
+}
+
+class _AddMembersPageState extends State<AddMembersPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
